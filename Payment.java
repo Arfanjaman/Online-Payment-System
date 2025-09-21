49da458b2248ce28ec5b59a82a13a27ef78056c4
Arfan <soraeshan6@gmail.com>
2025-09-21 14:44:15 +0600

    Commited from the brancH

---
+++ Payment.java
@@ -0,0 +1,5 @@
+// Abstract Payment class
+public abstract class Payment {
+    // Abstract method that must be implemented by all payment methods
+    public abstract void makePayment(double amount);
+}
